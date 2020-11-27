215cefc156c3c4343b853ab57ecc92fe2ffea9e0
Vishal Tyagi <vishaltyagi.9468@gmail.com>
2020-11-27 10:00:06 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$(function() {
+    "use strict";
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:20,
+        autoPlay:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:3
+            }
+        }
+    });
+});
